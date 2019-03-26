a4a0702c25a5436baed86e6598791a49268e94e8
Mathieu Lajoie <mlajoie03@gmail.com>
2019-03-25 20:21:44 -0400

    animated canvas, unsure

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
+import { goMovie, initCanvasFile } from '../../utility/custom-canvas';
 
 @Component({
     selector: 'ml-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
 
-    constructor() {
+    constructor(private elRef: ElementRef) {
     }
 
     ngOnInit() {
+    }
+
+    ngAfterViewInit() {
+        initCanvasFile(this.elRef.nativeElement.querySelector('#main-canvas'));
+        goMovie();
     }
 
 }
